5ee44c354fc7c099f5e722a7a30fa135de5b16cc
Ichraa99 <110057647+MarDevloper@users.noreply.github.com>
2024-09-11 16:02:22 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,18 +16,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16628744016"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'AW-16628744016');
-        </script>
       </head>
-      <body className='{`${inter.className}` text-white bg-black'>
+      <body className={`${inter.className} text-white bg-black`}>
         <Providers>
           <ToastContainer
             position="top-right"
@@ -41,9 +31,18 @@ export default function RootLayout({ children }) {
             pauseOnHover
             theme="dark"
           />
+
           {children}
         </Providers>
       </body>
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16628744016"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-16628744016');
+</script>
     </html>
   );
 }
