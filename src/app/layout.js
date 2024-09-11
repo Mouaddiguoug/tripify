045fbc8805cac00aade8b1020a49dfa91bf5b2f0
= <mouad.diguoug@evagrupo.com>
2024-09-11 15:44:05 +0100

    h

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,15 +16,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-       <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16628744016"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-16628744016');
-</script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16628744016"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'AW-16628744016');
+        </script>
       </head>
       <body className={`${inter.className} text-white bg-black`}>
         <Providers>
