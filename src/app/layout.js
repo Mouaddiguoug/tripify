35a820635849022e491b457b9b7540da816ac3ef
Ichraa99 <110057647+MarDevloper@users.noreply.github.com>
2024-09-12 18:06:08 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Providers } from "./data/provider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "@/components/home/footer";
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,16 +12,6 @@ export const metadata = {
   title: "Tripify",
   description: "Tripify",
 };
-import { GoogleTagManager } from '@next/third-parties/google'
-
-export default function RootLayout({children}: {children: React.ReactNode}) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-            <GoogleTagManager gtmId="GTM-XYZ" />
-        </html>
-     )
-}
 
 export default function RootLayout({ children }) {
   return (
@@ -45,6 +36,7 @@ export default function RootLayout({ children }) {
           {children}
         </Providers>
       </body>
+ <GoogleTagManager gtmId="GTM-XYZ" />
     </html>
   );
 }
