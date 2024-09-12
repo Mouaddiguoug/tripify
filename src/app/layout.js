2a90a1d0301f6f6f0a499ae5f399600cc09d51e4
Ichraa99 <110057647+MarDevloper@users.noreply.github.com>
2024-09-12 17:13:18 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,14 +35,6 @@ export default function RootLayout({ children }) {
           {children}
         </Providers>
       </body>
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16628744016"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-16628744016');
-</script>
     </html>
   );
 }
